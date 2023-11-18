1cb193f8160238e3556ea317aca73cf5f5b2721c
Navneet <navneetbodkhe@gmail.com>
2023-11-18 14:52:53 +0530

    recipe-api-completed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,10 @@
 """"
 Tests for models
 """
-
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 class ModelTests(TestCase):
     """Test models."""
@@ -45,3 +46,21 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+    def test_create_recipe(self):
+        """Test creating recipe is sucessful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'pass123',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description.'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
---
+++ app/recipe/urls.py
@@ -0,0 +1,20 @@
+""".Urls mapping for recipe App."""
+
+
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes',views.RecipeViewset)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
